0839e7b9757210600a9e028350921134cd35be15
hasan foraty <hasanforaty@gmail.com>
2023-09-05 11:33:30 +0330

    finishing command line chain

--- src/main/java/com/hasan/foraty/cruddemo/CruddemoApplication.java
+++ src/main/java/com/hasan/foraty/cruddemo/CruddemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -20,16 +21,61 @@ public class CruddemoApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDao studentDao){
         return runner->{
-//            createStudent(studentDao);
-//            readStudent(studentDao);
-//            queryForStudents(studentDao);
-//            queryForStudentsByLastName(studentDao);
-//            updateStudent(studentDao);
-//            deleteStudent(studentDao);
 
-//            deleteAllStudent(studentDao);
+            String input=null;
+            while (true){
+                System.out.println("Welcome to Student Command Line , for Exit please write /q");
+                System.out.println("new    : For adding new Student ");
+                System.out.println("update : For changing a Student ");
+                System.out.println("see    : For seeing a Student ");
+                System.out.println("seeAll : For seeing all of  Students");
+                Scanner in = new Scanner(System.in);
 
+                input= in.nextLine().toLowerCase();
+                switch (input){
+                    case "new":{
+                        createStudent(studentDao);
+                        break;
+                    }
+                    case "update":{
+                        updateStudent(studentDao);
+                        break;
+                    }
+                    case "see":{
+                        readStudent(studentDao);
+                        break;
+                    }
+                    case "seeall":{
+                        queryForStudents(studentDao);
+                        break;
+                    }
+                    case "/q":{
+                        return;
+
+                    }
+                    default:{
+                        System.out.println("Please choose an option from one presented to you");
+                    }
+                }
+            }
         };
+    }
+
+
+
+    private Student getStudent(){
+        Student student = new Student();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please Enter first name");
+        String  firstName =in.nextLine();
+        student.setFirstName(firstName);
+        System.out.println("Please Enter last name");
+        String  lastName =in.nextLine();
+        student.setLastName(lastName);
+        System.out.println("Please Enter email");
+        String  email =in.nextLine();
+        student.setEmail(email);
+        return  student;
     }
 
     private void deleteAllStudent(StudentDao studentDao) {
@@ -52,13 +98,14 @@ public class CruddemoApplication {
     }
 
     private void updateStudent(StudentDao studentDao) {
-        Student theStudent = studentDao.findById(1);
+        System.out.println("Please Enter id of Student");
+        String  id = new Scanner(System.in).nextLine();
+        Student theStudent = studentDao.findById(Integer.parseInt(id));
         System.out.println("Updating student ......");
-        theStudent.setFirstName("Dummy");
-        studentDao.update(theStudent);
+        Student newStudent = getStudent();
+        newStudent.setId(theStudent.getId());
+        studentDao.update(newStudent);
         System.out.println("checking.....");
-        Student changedStudent = studentDao.findById(1);
-        System.out.println(changedStudent.toString().equals(theStudent.toString()));
     }
 
     private void queryForStudentsByLastName(StudentDao studentDao) {
@@ -89,7 +136,7 @@ public class CruddemoApplication {
      */
     private void createStudent(StudentDao studentDao){
         System.out.println("Creating new student object.....");
-        Student tempStudent = new Student("Paul","Doe","PaulDoe@Gmail.com");
+        Student tempStudent = getStudent();
         System.out.println("Saving the student ...."+tempStudent);
         studentDao.save(tempStudent);
         System.out.println("Saved , student id : "+tempStudent.getId());
@@ -100,12 +147,9 @@ public class CruddemoApplication {
      * @param studentDao dao of class student
      */
     private void readStudent(StudentDao studentDao){
-        System.out.println("Creating new Student object.....");
-        Student tempStudent = new Student("Daffy","Duck","Millioner@Gmail.com");
-        System.out.println("Saving new Student.....");
-        studentDao.save(tempStudent);
-        System.out.println("Retrieve student with id : "+tempStudent.getId());
-        System.out.println("Found the student "+studentDao.findById(tempStudent.getId()));
+        System.out.println("Please Enter student id");
+        String  id = new Scanner(System.in).nextLine();
+        System.out.println(studentDao.findById(Integer.parseInt(id)));
     }
 
 
